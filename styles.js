e718396e7e62e09120dc0574dfba21d76f7bc632
GuNShOtzZ <kevinloeding@outlook.de>
2024-01-15 11:45:26 +0100

    Format body

--- styles.js
+++ styles.js
@@ -5,10 +5,24 @@ export default createGlobalStyle`
   *::before,
   *::after {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
 
   body {
-    margin: 0;
     font-family: system-ui;
+    margin: 1rem 0rem 0rem 1rem;
+  }
+
+  header {
+    margin-bottom: 2rem;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  h1 {
+    text-align: center;
   }
 `;
