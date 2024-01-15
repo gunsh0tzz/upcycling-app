9b5ed0209a17456b7438723950b47ea568dbef03
GuNShOtzZ <kevinloeding@outlook.de>
2024-01-15 10:09:38 +0100

    Fixed error in next config

--- next.config.js
+++ next.config.js
@@ -9,33 +9,33 @@ const nextConfig = {
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
-      images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "images.unsplash.com",
-            port: "",
-          },
-          {
-            protocol: "https",
-            hostname: "unsplash.com",
-            port: "",
-          },
-          {
-            protocol: "https",
-            hostname: "hotpot.ai",
-            port: "",
-          },
-          {
-            protocol: "https",
-            hostname: "hotpotmedia.s3.us-east-2.amazonaws.com",
-            port: "",
-          },
-        ],
-      },
     });
 
     return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "hotpot.ai",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "hotpotmedia.s3.us-east-2.amazonaws.com",
+        port: "",
+      },
+    ],
   },
 };
 
