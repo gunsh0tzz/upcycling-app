b9ab2a9e5063997e3ca134c0013e2a3647fc59e4
GuNShOtzZ <kevinloeding@outlook.de>
2024-01-15 11:44:59 +0100

    Create landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,49 @@
+console.clear();
+
+import { useState } from "react";
+import styled from "styled-components";
+
+import { ideas } from "@/lib/db";
+import Card from "../components/Card";
+import Header from "@/components/Header";
+
+const defaultIdeas = ideas;
+
+const CardContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  list-style: none;
+  padding: 0;
+  margin-top: 1rem;
+`;
+
+const CardListItem = styled.li`
+  flex: 0 0 calc(50% - 1rem);
+  max-width: calc(50% - 1rem);
+  box-sizing: border-box;
+  border: 1px solid black;
+  border-radius: 0.5rem;
+  padding: 1rem;
+`;
+
 export default function HomePage() {
+  const [ideas, setIdeas] = useState(defaultIdeas);
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Header />
+      <CardContainer>
+        {ideas.map((idea) => (
+          <CardListItem key={idea.id}>
+            <Card
+              image={idea.image}
+              title={idea.title}
+              hashtags={idea.hashtags}
+            />
+          </CardListItem>
+        ))}
+      </CardContainer>
     </div>
   );
 }
