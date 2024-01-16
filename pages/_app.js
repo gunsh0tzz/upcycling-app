cb52ff77e68de51d7772a4492309893961298b59
Sarah Morgen <sarah.morgen@outlook.de>
2024-01-16 11:39:15 +0100

    add form functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import GlobalStyle from "../styles";
+import { ideas as defaultIdeas } from "@/lib/db";
+import { useState } from "react";
+
 
 export default function App({ Component, pageProps }) {
+  const [ideas, setIdeas] = useState(defaultIdeas);
+  function addIdea(newIdea) {
+    newIdea.id = uuidv4();
+    setIdeas([newIdea, ...ideas]);
+  }
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} ideas={ideas} addIdea={addIdea}/>
     </>
   );
 }
--- pages/create.js
+++ pages/create.js
@@ -1,10 +1,14 @@
 import Form from "@/components/Form"
 import Header from "@/components/Header"
 
-export default function Create (){
+export default function Create ({addIdea}){
+    function handleSubmit(newIdea) {
+        addIdea(newIdea);
+      }
 return (<>
 <Header/>
-<Form/>
+<Form onSubmit={handleSubmit}/>
+
 </>) 
 
 }
