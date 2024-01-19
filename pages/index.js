4b23180c9a6ae6e9917caa1e3ecade5cdbbf25bd
Kevin <68171149+kevinldg@users.noreply.github.com>
2024-01-19 15:37:21 +0100

    Revert "User Story 8 - Search Bar"

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import styled from "styled-components";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import Fuse from "fuse.js";
 
 import Card from "../components/Card";
-import Searchbar from "@/components/Searchbar";
-
-import { ideas as defaultIdeas } from "@/lib/db";
 
 const CardList = styled.ul`
   display: flex;
@@ -40,81 +35,22 @@ export default function HomePage({
   onToggleFavourites,
   favouriteIdeas,
 }) {
-  const [suggestions, setSuggestions] = useState([]);
-  const [searchResults, setSearchResults] = useState(
-    ideas.map((idea) => ({ item: idea }))
-  );
-  const [searchValue, setSearchValue] = useState("");
-
-  const fuse = new Fuse(defaultIdeas, {
-    keys: ["hashtags", "title"],
-  });
-
-  function handleClickEvent(value) {
-    setSearchValue(value);
-    setSearchResults(fuse.search(value));
-    console.log(searchValue);
-  }
-  function handleInputChange(item) {
-    setSuggestions(fuse.search(item));
-  }
-
   return (
     <div>
-      <Searchbar
-        suggestions={suggestions}
-        onInputChange={handleInputChange}
-        searchValue={searchValue}
-        onClickEvent={handleClickEvent}
-        setSearchValue={setSearchValue}
-      />
       <CardList>
-        {suggestions.length > 0 && searchValue
-          ? suggestions.map((suggestion) => (
-              <CardListItem key={suggestion.item.id}>
-                <Card
-                  image={suggestion.item.image}
-                  title={suggestion.item.title}
-                  hashtags={suggestion.item.hashtags}
-                  onToggleFavourites={onToggleFavourites}
-                  isFavourite={suggestion.item.isFavourite}
-                  id={suggestion.item.id}
-                />
-                <StyledLink href={`/ideaDetails/${suggestion.item.id}`}>
-                  See More
-                </StyledLink>
-              </CardListItem>
-            ))
-          : ideas.map((idea) => (
-              <CardListItem key={idea.id}>
-                <Card
-                  image={idea.image}
-                  title={idea.title}
-                  hashtags={idea.hashtags}
-                  onToggleFavourites={onToggleFavourites}
-                  isFavourite={idea.isFavourite}
-                  id={idea.id}
-                />
-                <StyledLink href={`/ideaDetails/${idea.id}`}>
-                  See More
-                </StyledLink>
-              </CardListItem>
-            ))}
-        {/* {searchResults.map((searchResult) => (
-          <CardListItem key={searchResult.item.id}>
+        {ideas.map((idea) => (
+          <CardListItem key={idea.id}>
             <Card
-              image={searchResult.item.image}
-              title={searchResult.item.title}
-              hashtags={searchResult.item.hashtags}
+              image={idea.image}
+              title={idea.title}
+              hashtags={idea.hashtags}
               onToggleFavourites={onToggleFavourites}
-              isFavourite={searchResult.item.isFavourite}
-              id={searchResult.item.id}
+              isFavourite={idea.isFavourite}
+              id={idea.id}
             />
-            <StyledLink href={`/ideaDetails/${searchResult.item.id}`}>
-              See More
-            </StyledLink>
+            <StyledLink href={`/ideaDetails/${idea.id}`}>See More</StyledLink>
           </CardListItem>
-        ))} */}
+        ))}
       </CardList>
     </div>
   );
