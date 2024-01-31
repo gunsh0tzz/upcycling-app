7fbbd962893060e920ef5cd9eb679abe0fceef74
Jore Averbeck <noa.averbeck@gmail.com>
2024-01-31 19:26:02 +0100

    add searchbar margin

--- components/Searchbar/index.js
+++ components/Searchbar/index.js
@@ -6,16 +6,13 @@ import {
   faArrowsRotate,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import Link from "next/link";
-
 const Container = styled.div`
   position: relative;
   width: 65%;
   height: 20%;
   align-self: center;
   display: flex;
-
   justify-content: center;
   background-color: #fafafa;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
@@ -24,52 +21,51 @@ const Container = styled.div`
     max-width: 400px;
   }
 `;
-
 const StyledSearchbar = styled.input`
   padding: 0.5rem;
   border: none;
   width: 100%;
 `;
-
 const StyledSearchButton = styled.button`
   background-color: #a97bb5;
   padding: 0.4rem;
   border-radius: 5rem;
   border: none;
 `;
-
 const StyledListItem = styled.li`
   list-style: none;
-
-  width: 15rem;
+  width: 100%;
   padding: 0.25rem;
-
   &:hover {
     background-color: rgba(0, 0, 0, 0.125);
   }
 `;
-
 const StyledLink = styled(Link)`
   text-decoration: none;
   color: black;
-
   &:hover {
     text-decoration: underline;
   }
 `;
 const StyledSearchPreview = styled.ul`
   position: absolute;
-  top: 162px;
-  left: 10px;
-  width: 50%;
+  top: 102%;
+  left: 0%;
+  width: 65%;
   justify-content: center;
   background-color: #fafafa;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
   border-radius: 0.8rem;
   padding: 0.5rem;
   z-index: 1;
+  @media (min-width: 600px) {
+    max-width: 400px;
+  }
 `;
-
+const MainContainer = styled.div`
+  position: relative;
+  margin-left: 2rem;
+`;
 export default function Searchbar({
   suggestions,
   onInputChange,
@@ -79,7 +75,6 @@ export default function Searchbar({
   const [inputValue, setInputValue] = useState("");
   const [showSuggestions, setShowSuggestions] = useState(true);
   const router = useRouter();
-
   function handleChangeEvent(e) {
     onInputChange(e.target.value);
     setInputValue(e.target.value);
@@ -93,16 +88,14 @@ export default function Searchbar({
     setShowSuggestions(false);
     onClickEvent(inputValue);
   }
-
   return (
-    <>
+    <MainContainer>
       <Container>
         <StyledSearchbar
           placeholder="Search for titles or hashtags"
           value={inputValue}
           onChange={handleChangeEvent}
         />
-
         {searchValue ? (
           <StyledSearchButton onClick={handleReset}>
             <FontAwesomeIcon icon={faArrowsRotate} />
@@ -113,16 +106,17 @@ export default function Searchbar({
           </StyledSearchButton>
         )}
       </Container>
-      <StyledSearchPreview>
-        {showSuggestions &&
-          suggestions.map(({ item }, index) => (
+      {showSuggestions && suggestions.length > 0 && (
+        <StyledSearchPreview>
+          {suggestions.map(({ item }, index) => (
             <StyledListItem key={index}>
               <StyledLink href={`/ideaDetails/${item._id}`}>
                 {item.title}
               </StyledLink>
             </StyledListItem>
           ))}
-      </StyledSearchPreview>
-    </>
+        </StyledSearchPreview>
+      )}
+    </MainContainer>
   );
 }
