a30fd36350c0e96f61baf2136549cd1df5ee266c
GuNShOtzZ <kevinloeding@outlook.de>
2024-01-31 15:02:36 +0100

    Protect create page

--- pages/create.js
+++ pages/create.js
@@ -1,9 +1,15 @@
 import Form from "@/components/Form";
 import { mutate } from "swr";
 import { useRouter } from "next/router";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Create({ addIdea }) {
+  const { data: session, status } = useSession();
   const router = useRouter();
+
+  if (status !== "authenticated") {
+    return <h1>Access denied. You have to be logged in to view this page.</h1>;
+  }
 
   async function onSubmit(data) {
     const response = await fetch("/api/ideas", {
