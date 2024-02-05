df88d3e9a5dc721a7e34679b31e85f27ffcb12ef
GuNShOtzZ <kevinloeding@outlook.de>
2024-02-05 16:42:07 +0100

    Complete the profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import styled from "styled-components";
+import { useSession } from "next-auth/react";
+import AccessDeniedMessage from "@/lib/AccessDeniedMessage";
+import AuthButton from "@/components/AuthButton/AuthButton";
 
 const StyledContainer = styled.div`
   display: flex;
@@ -7,20 +10,34 @@ const StyledContainer = styled.div`
   align-items: center;
   gap: 1rem;
 
+  margin-top: 1rem;
   text-align: center;
 `;
 
 const StyledImage = styled(Image)`
   border-radius: 100%;
-  border: 0.25rem solid black;
+  border: 0rem solid black;
+  box-shadow: 0 0 0.5rem black;
 `;
 
 export default function ProfilePage() {
+  const { data: session, status } = useSession();
+
+  if (status !== "authenticated") {
+    return <AccessDeniedMessage />;
+  }
+
   return (
     <StyledContainer>
-      <StyledImage src="/avatar.jpg" alt="Avatar" width={192} height={192} />
-      <h1>Username</h1>
-      <p>username@username.de</p>
+      <StyledImage
+        src={session.user.image}
+        alt="Avatar"
+        width={192}
+        height={192}
+      />
+      <h1>{session.user.name}</h1>
+      <p>{session.user.email}</p>
+      <AuthButton />
     </StyledContainer>
   );
 }
