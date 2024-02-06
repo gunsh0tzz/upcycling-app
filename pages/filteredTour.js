8b5a66f744bfc07965b4e09f23f6ef7ea826597a
Jore Averbeck <noa.averbeck@gmail.com>
2024-02-06 12:18:12 +0100

    add functionality

--- pages/filteredTour.js
+++ pages/filteredTour.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import Fuse from "fuse.js";
 import { useState } from "react";
+import Card from "@/components/Card";
 
-export default function FilteredTour({ ideas }) {
+export default function FilteredTour({
+  ideas,
+  onToggleFavourites,
+  favouriteIdeas,
+}) {
   const [selectedMaterials, setSelectedMaterials] = useState([]);
   const [selectedForms, setSelectedForms] = useState([]);
+  const [selectedDifficulties, setSelectedDifficulties] = useState([]);
+  const [filteredIdeas, setFilteredIdeas] = useState([]);
 
   const fuse = new Fuse(ideas, {
     keys: ["hashtags"],
@@ -23,20 +30,25 @@ export default function FilteredTour({ ideas }) {
       setSelectedForms([...selectedForms, form]);
     }
   };
+  const handleDifficultyButtonClick = (form) => {
+    if (!selectedDifficulties.includes(form)) {
+      setSelectedDifficulties([...selectedDifficulties, form]);
+    }
+  };
 
   const searchMaterials = () => {
     const filteredIdeas = ideas.filter((idea) => {
       return (
         idea.hashtags.some((tag) => selectedMaterials.includes(tag)) &&
-        idea.hashtags.some((tag) => selectedForms.includes(tag))
+        idea.hashtags.some((tag) => selectedForms.includes(tag)) &&
+        idea.hashtags.some((tag) => selectedDifficulties.includes(tag))
       );
     });
-
-    console.log(filteredIdeas);
+    setFilteredIdeas(filteredIdeas);
   };
 
-  console.log("Materials" + selectedMaterials);
-  console.log("Forms" + selectedForms);
+  console.log(selectedMaterials);
+
   return (
     <>
       <section>
@@ -49,7 +61,7 @@ export default function FilteredTour({ ideas }) {
             height={100}
           />
         </button>
-        <button onClick={() => handleMaterialButtonClick("PET")}>
+        <button onClick={() => handleMaterialButtonClick("plastic")}>
           <Image
             src="https://images.unsplash.com/photo-1576037728058-ab2c538ac8d0?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="plastic"
@@ -68,6 +80,30 @@ export default function FilteredTour({ ideas }) {
         <button onClick={() => handleMaterialButtonClick("glass")}>
           <Image
             src="https://images.unsplash.com/photo-1542661206-03e9001692de?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="glass"
+            width={100}
+            height={100}
+          />
+        </button>
+        <button onClick={() => handleMaterialButtonClick("metal")}>
+          <Image
+            src="https://images.unsplash.com/photo-1597227093570-eccc70d8a416?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="glass"
+            width={100}
+            height={100}
+          />
+        </button>
+        <button onClick={() => handleMaterialButtonClick("paper")}>
+          <Image
+            src="https://images.unsplash.com/photo-1601662528567-526cd06f6582?q=80&w=2630&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="glass"
+            width={100}
+            height={100}
+          />
+        </button>
+        <button onClick={() => handleMaterialButtonClick("textile")}>
+          <Image
+            src="https://images.unsplash.com/photo-1534639077088-d702bcf685e7?q=80&w=2193&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="glass"
             width={100}
             height={100}
@@ -110,8 +146,55 @@ export default function FilteredTour({ ideas }) {
           />
         </button>
       </section>
+      <section>
+        <h2>Difficulty</h2>
+        <button onClick={() => handleDifficultyButtonClick("easy")}>
+          <Image
+            src="https://images.unsplash.com/photo-1588421357574-87938a86fa28?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="easy"
+            width={100}
+            height={100}
+          />
+        </button>
+        <button onClick={() => handleDifficultyButtonClick("medium")}>
+          <Image
+            src="https://images.unsplash.com/photo-1508898578281-774ac4893c0c?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="medium"
+            width={100}
+            height={100}
+          />
+        </button>
+        <button onClick={() => handleDifficultyButtonClick("difficult")}>
+          <Image
+            src="https://images.unsplash.com/photo-1544914379-806667cd9489?q=80&w=2531&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="difficult"
+            width={100}
+            height={100}
+          />
+        </button>
+        <button onClick={() => handleDifficultyButtonClick("profi")}>
+          <Image
+            src="https://images.unsplash.com/photo-1548504769-900b70ed122e?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="profi"
+            width={100}
+            height={100}
+          />
+        </button>
+      </section>
 
       <button onClick={searchMaterials}>Results</button>
+
+      {filteredIdeas.length > 0 && (
+        <Card
+          image={filteredIdeas[0].image}
+          cover={filteredIdeas[0].cover}
+          title={filteredIdeas[0].title}
+          hashtags={filteredIdeas[0].hashtags}
+          onToggleFavourites={onToggleFavourites}
+          favouriteIdeas={favouriteIdeas}
+          id={ideas[0]._id}
+        />
+      )}
     </>
   );
 }
